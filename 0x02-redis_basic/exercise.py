f314ddc72675bd0ff212bf88e1be2b6e2817d626
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-25 00:12:40 +0000

    recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Redis Basics Module"""
 
 import redis
-from typing import Union
+from typing import Any, Optional, Union, Callable
 import uuid
 
 
@@ -19,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable[[Any], Any] = None) -> Any:
+        """Takes key argument and returns data in desired format"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        else:
+            return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """parametrize Cache.get with correct conversion func"""
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """parametrize Cache.get with correct conversion func"""
+        return self.get(key, fn=int)
